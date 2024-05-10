227c7609f3e9a9c16de24d5bc0be3c52c5a73a85
Salman Emad <salmanemad2003@gmail.com>
2024-05-10 23:58:20 +0300

    changed to flask

--- main.py
+++ main.py
@@ -1,30 +1,22 @@
-# main.py
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse 
-from fastapi.staticfiles import StaticFiles
+from flask import Flask, render_template
 
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+app = Flask(__name__)
 
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    context = {"request": request, "message": "Hello, FastAPI!"}
-    return templates.TemplateResponse("home.html", {"request": request, **context})
+@app.route("/")
+def home():
+    return render_template("home.html")
 
-@app.get("/services", response_class=HTMLResponse)
-async def services(request: Request):
-    context = {"request": request, "message": "Hello, FastAPI!"}
-    return templates.TemplateResponse("services.html", {"request": request, **context})
+@app.route("/services")
+def services():
+    return render_template("services.html")
 
-@app.get("/portfolio", response_class=HTMLResponse)
-async def portfolio(request: Request):
-    context = {"request": request, "message": "Hello, FastAPI!"}
-    return templates.TemplateResponse("portfolio.html", {"request": request, **context})
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
 
-@app.get("/about", response_class=HTMLResponse)
-async def about(request: Request):
-    context = {"request": request, "message": "Hello, FastAPI!"}
-    return templates.TemplateResponse("about.html", {"request": request, **context})
-# Mount the static files directory to /static
-app.mount("/static", StaticFiles(directory="static"), name="static")
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
